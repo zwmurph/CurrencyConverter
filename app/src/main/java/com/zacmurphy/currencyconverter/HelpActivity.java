0b7fcee58f08c24ce15408f9bd6727310fa15f92
Zac <zacmoonbase@gmai.com>
2018-01-01 17:21:22 +0000

    Gave the help & about activity content

--- app/src/main/java/com/zacmurphy/currencyconverter/HelpActivity.java
+++ app/src/main/java/com/zacmurphy/currencyconverter/HelpActivity.java
@@ -1,7 +1,9 @@
 package com.zacmurphy.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import static com.zacmurphy.currencyconverter.Currency.currenciesList;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        TextView exchangeRateDateView = (TextView) findViewById(R.id.exchangeRateDate);
+        exchangeRateDateView.setText(currenciesList.get(0).getDateOfExchange());
     }
 }
