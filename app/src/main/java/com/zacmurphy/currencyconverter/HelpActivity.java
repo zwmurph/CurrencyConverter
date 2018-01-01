13dda50c7bfbac799079f10d4a79ec0feef0cbfb
Zac <zacmoonbase@gmai.com>
2018-01-01 17:47:16 +0000

    Gave the help & about activity content

--- app/src/main/java/com/zacmurphy/currencyconverter/HelpActivity.java
+++ app/src/main/java/com/zacmurphy/currencyconverter/HelpActivity.java
@@ -3,16 +3,26 @@ package com.zacmurphy.currencyconverter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
 import static com.zacmurphy.currencyconverter.Currency.currenciesList;
 
 public class HelpActivity extends AppCompatActivity {
+
+    //Tag for the log messages
+    private static final String LOG_TAG = HelpActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        //Create a constructor for the TextView
         TextView exchangeRateDateView = (TextView) findViewById(R.id.exchangeRateDate);
-        exchangeRateDateView.setText(currenciesList.get(0).getDateOfExchange());
+
+        //String to Date conversion in Java with dd-MM-yyyy format e.g. "14-09-2011"
+        String dateOfExchange = currenciesList.get(0).getDateOfExchange();
+
+        //Set the date on the TextView
+        exchangeRateDateView.setText(dateOfExchange);
     }
 }
